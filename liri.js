da8003238a9f4a99d84e13c94e963a4367e6f310
Luis Villalobos <vlobos323@gmail.com>
2019-08-26 21:38:00 -0700

    added "bands in town" functionality

--- liri.js
+++ liri.js
@@ -1,8 +1,14 @@
 require("dotenv").config();
 
-var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
 
 let input = process.argv[2];
+
+if(input === 'concert-this'){
+    concert();
+} else if(input === 'spotify-this-song'){
+    spotifyArtists();
+}
 
 function concert(){
     let artist = process.argv.slice(3).join(' ');
@@ -12,7 +18,27 @@ function concert(){
  
     axios.get(url)
     .then(function (response) {
-        console.log(response);
+        let jsonData = response.data[0];
+        console.log(jsonData.venue.name);
+        console.log(jsonData.venue.city);
+        console.log(jsonData.datetime);
     })
+};
+
+function spotifyArtists() {
+    var Spotify = require('node-spotify-api');
+    let song = process.argv.slice(3).join(' ');
+ 
+    var spotify = new Spotify({
+        id: '3d419acd23004f0f89dabf71dc9d55eb',
+        secret: 'a8ccb7135445419385d5ffeb43efa3ad'
+    });
+    
+    spotify.search({ type: 'track', query: song }, function(err, data) {
+    if (err) {
+        return console.log('Error occurred: ' + err);
+    }
+    
+    console.log(data); 
+    });
 }
-concert();
